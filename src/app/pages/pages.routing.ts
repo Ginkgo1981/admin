ceb0e7909242b952770cc31f347fc8a82b50d24e
jian chen <jian.chen@skymatters.com>
2017-03-10 16:36:24 +0800

    remove unused component

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -5,28 +5,14 @@ import { ModuleWithProviders } from '@angular/core';
 
 // export function loadChildren(path) { return System.import(path); };
 
+// noinspection TypeScriptValidateTypes
 export const routes: Routes = [
-  {
-    path: 'login',
-    loadChildren: 'app/pages/login/login.module#LoginModule'
-  },
-  {
-    path: 'register',
-    loadChildren: 'app/pages/register/register.module#RegisterModule'
-  },
   {
     path: 'pages',
     component: Pages,
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', loadChildren: 'app/pages/dashboard/dashboard.module#DashboardModule' },
-      { path: 'editors', loadChildren: 'app/pages/editors/editors.module#EditorsModule' },
-      { path: 'components', loadChildren: 'app/pages/components/components.module#ComponentsModule' },
-      { path: 'charts', loadChildren: 'app/pages/charts/charts.module#ChartsModule' },
-      { path: 'ui', loadChildren: 'app/pages/ui/ui.module#UiModule' },
-      { path: 'forms', loadChildren: 'app/pages/forms/forms.module#FormsModule' },
-      { path: 'tables', loadChildren: 'app/pages/tables/tables.module#TablesModule' },
-      { path: 'maps', loadChildren: 'app/pages/maps/maps.module#MapsModule' },
       { path: 'students',  loadChildren: 'app/pages/students/students.module#StudentsModule' }
 
     ]
